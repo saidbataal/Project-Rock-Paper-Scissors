6e452382181fdd28de9771bc9ae06b5e58760f2c
saidbataal <saidmt092018@gmail.com>
2025-04-11 09:07:48 +0100

    Add function to generate random choice (rock, paper, or scissors)

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+function getComputerChoice(){
+let result = Math.floor(Math.random() *3)
+    if(result === 0){return "rock"}
+    else if(result === 1){return "paper"}
+else{return "scissors"}
+}
+
+
